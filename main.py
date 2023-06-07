65f55d6b399f83c60048e6859f5908f7f971664a
Farimah Chamanzadeh <121865417+farimaahchz@users.noreply.github.com>
2023-06-07 13:58:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ print("Current working directory: {0}".format(cwd))
 
 # Print the type of the returned object
 print("os.getcwd() returns an object of type: {0}".format(type(cwd)))
-os.chdir('/Users/farimaahchz/Winc/files')
+#os.chdir('/Users/farimaahchz/Winc/files')
 # Print the current working directory
 print("Current working directory: {0}".format(os.getcwd()))
 
